3fc76eef9a13418b9757f8004e69f365f644f92a
ibrahimeatspie <104ibrahimshah@gmail.com>
2022-12-04 22:00:35 -0800

    imported styling template

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,9 +1,103 @@
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
 function App() {
   return (
     
-    <h1>asdasd</h1>
+    <>
+      <nav class="navbar navbar-dark bg-primary">
+        <a class="navbar-brand mx-4" href=""><h1>React To-do List</h1></a>
+      </nav>
+    <div class="container">
+        <div class="row my-5">
+            <div class="col-8">
+                <input type="text" id="text" class="form-control form-control-lg" placeholder="Enter task" />
+            </div>
+            <div class="col">
+                <button class="btn btn-primary btn-lg w-100">Add</button>
+            </div>
+            
+        </div>
+      <div class="container" id="tasks">
+
+        <div class="row mx-1 mb-2 mt-2">
+          <div class="col-10 form-check">
+            <input class="form-check-input checkbox-xl" type="checkbox" value="" id="checkbox-3" checked/>
+            <label class="mx-2 form-check-label large-label " for="checkbox-3"><span class="strike-through">Extra large checkbox hi my name is ibrahim and iw as just wodnering how you are feeling today</span></label>
+          </div>
+          <button class="col-2 btn btn-primary btn-sm">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+            <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+          </svg>
+        </button>
+      </div>
+
+      <div class="row mx-1 mb-2 mt-2">
+        <div class="col-10 form-check">
+          <input class="form-check-input checkbox-xl" type="checkbox" value="" id="checkbox-3" checked/>
+          <label class="mx-2 form-check-label large-label " for="checkbox-3"><span class="strike-through">Extra large checkbox</span></label>
+        </div>
+        <button class="col-2 btn btn-primary btn-sm">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+          <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+          <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+        </svg>
+      </button>
+    </div>
+
+    <div class="row mx-1 mb-2 mt-2">
+      <div class="col-10 form-check">
+        <input class="form-check-input checkbox-xl" type="checkbox" value="" id="checkbox-3" checked/>
+        <label class="mx-2 form-check-label large-label " for="checkbox-3"><span class="strike-through">Extra large checkbox</span></label>
+      </div>
+      <button class="col-2 btn btn-primary btn-sm">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+        <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+      </svg>
+    </button>
+  </div>
+
+  <div class="row mx-1 mb-2 mt-2">
+    <div class="col-10 form-check">
+      <input class="form-check-input checkbox-xl" type="checkbox" value="" id="checkbox-3" checked/>
+      <label class="mx-2 form-check-label large-label " for="checkbox-3"><span class="strike-through">Extra large checkbox</span></label>
+    </div>
+    <button class="col-2 btn btn-primary btn-sm">
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+      <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+      <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+    </svg>
+  </button>
+</div>
+
+      
+
+    </div>
+
+    
+
+      </div>
+        
+        
+        
+            
+
+           
+
+
+        
+
+        
+            
+        
+       
+        
+
+
+    
+    </>
   );
 }
 
