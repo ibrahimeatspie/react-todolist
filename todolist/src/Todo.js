7c3af0d74c938634b17167f88ac984245a6bfae8
ibrahimeatspie <104ibrahimshah@gmail.com>
2022-12-15 14:17:43 -0600

    finished deletion

--- todolist/src/Todo.js
+++ todolist/src/Todo.js
@@ -9,6 +9,32 @@ export default function Todo({task, checked, id, setTodos}) {
   const [isChecked, setIsChecked] = useState(checked)
 
   
+  function handleDelete(){
+    //console.log("Delet")
+    setTodos(prev=>{
+      console.log(prev)
+      let prevTodos = [...prev]
+      let newTodos = []
+      let todoToDel;
+      //delete the current todo
+      for (let i = 0; i<prevTodos.length;i++){
+        //console.log(prevTodos[i])
+        
+        if (prevTodos[i].id == id){
+          continue
+         //console.log("match found")
+
+        }else{
+          newTodos.push(prevTodos[i])
+        }
+      }
+       // console.log(todoToDel)
+        
+        return newTodos
+      
+      
+    })
+  }
 
   function changeCheck(){
     console.log(id)
@@ -46,7 +72,7 @@ export default function Todo({task, checked, id, setTodos}) {
               <input className="form-check-input checkbox-xl" type="checkbox" value="" id="checkbox-3" onChange={changeCheck} checked/>
               <label className="mx-2 form-check-label large-label " for="checkbox-3"><span className="strike-through">{task}</span></label>
             </div>
-            <button className="col-2 btn btn-primary btn-sm">
+            <button className="col-2 btn btn-primary btn-sm" onClick={handleDelete}>
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
               <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
               <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
@@ -67,7 +93,7 @@ export default function Todo({task, checked, id, setTodos}) {
               <input className="form-check-input checkbox-xl" type="checkbox" value="" id="checkbox-3" onChange={changeCheck}/>
               <label className="mx-2 form-check-label large-label " for="checkbox-3">{task}</label>
             </div>
-            <button className="col-2 btn btn-primary btn-sm">
+            <button className="col-2 btn btn-primary btn-sm" onClick={handleDelete}>
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
               <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
               <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
